fcfb6695fab6746f3070948448cb670722d4e820
Lazare Kvirtia <lazarekvirtia@gmail.com>
2021-10-22 19:35:07 +0400

    password change (quiz 3)

--- app/src/main/java/ge/msda/myapplication/MainActivity.java
+++ app/src/main/java/ge/msda/myapplication/MainActivity.java
@@ -52,6 +52,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        passwordChangeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, PasswordChangeActivity.class));
+                finish();
+            }
+        });
+
+
     }
 
 }
