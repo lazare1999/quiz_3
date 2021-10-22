2656f9069cbe520180ccc1ee9ca1d43b1f21d879
Lazare Kvirtia <lazarekvirtia@gmail.com>
2021-10-22 18:50:37 +0400

    Initial commit

---
+++ app/src/main/java/ge/msda/myapplication/PasswordChangeActivity.java
@@ -0,0 +1,14 @@
+package ge.msda.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PasswordChangeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password_change);
+    }
+}
